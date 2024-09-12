26d63367a23d2c4bffd3394dba31c8e28aa18bd1
JimPolder <302101233@student.rocmondriaan.nl>
2024-09-12 09:30:02 +0200

    variabelen

--- VariabelenOpdracht/JimVariabelen.js
+++ VariabelenOpdracht/JimVariabelen.js
@@ -1,1 +1,30 @@
+// Jim variabelen opdracht
+
+const start = ("start")
+//hiermee kan je op de startknop drukken
+
+const username = ("naam")
+//hiermee wordt de gebruikersnaam aangemaakt via een prompt
+
+const diff = ("difficulty")
+//hiermee wordt de moeilijkheids gekozen
+
+const chosennum = ("chosen number")
+//het nummer dat de computer neemt waar het spel om draait
+
+const visibnum = ("visible number")
+//het nummer dat de speler te zien krijgt waarbij ze moeten raden of het nummer van de computer hoger of lager is
+
+const hoger = ("hoger")
+//een knop voor de 'hoger' optie tijdens het spel
+
+const lager = ("lager")
+//een knop voor de 'lager' optie tijdens het spel
+
+const score = ("score")
+//hiermee wordt de score aangegeven
+
+const lives = ("lives")
+//hiermee worden de hoeveelheid levens aangegeven
+
 
