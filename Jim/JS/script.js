8de78e066aa2033db8c80da8ad0e497fcd50cf93
Jim Polder <302101233@student.rocmondriaan.nl>
2024-09-20 12:41:04 +0200

    directories

---
+++ Jim/JS/script.js
@@ -0,0 +1,4 @@
+//username prompt
+let Username = prompt("Wat is je naam?", "naam");
+document.getElementById("usr").innerHTML = "Username";
+
--- Jim/script.js
+++
@@ -1,2 +0,0 @@
-//username prompt
-let Username = prompt("Wat is je naam?", "naam")
