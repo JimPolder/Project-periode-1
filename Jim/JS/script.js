fc26b16c8299db3a777e8d81cecc41e5a2915ca6
JimPolder <302101233@student.rocmondriaan.nl>
2024-09-22 22:10:56 +0200

    math.random opdracht

--- Jim/JS/script.js
+++ Jim/JS/script.js
@@ -2,3 +2,9 @@
 let Username = prompt("Wat is je naam?", "naam");
 document.getElementById("usr").innerHTML = "Username";
 
+let CompNumber = Math.floor(Math.random() * 10);
+let RandomUserNumber = Math.floor(Math.random() * 10);
+
+if(CompNumber = RandomUserNumber){
+    console.log("gelijk")
+}
