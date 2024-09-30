73ff9577d5eb8ddca16e11141cd5bf64f6d1727f
JimPolder <302101233@student.rocmondriaan.nl>
2024-09-30 09:02:52 +0200

    small changes

--- Jim/JS/script.js
+++ Jim/JS/script.js
@@ -1,12 +1,3 @@
 //username prompt
 let Username = prompt("Wat is je naam?", "naam");
 document.getElementById("usr").innerHTML = "Username";
-
-let CompNumber = Math.floor(Math.random() * 10);
-let RandomUserNumber = Math.floor(Math.random() * 10);
-
-if(CompNumber == RandomUserNumber){
-    console.log("User won")
-} else {
-    console.log("Comp won")
-}
